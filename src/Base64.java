42e85df53b338d96d31632c5650ab4578b40c8af
caesar.chen@onebank.com.cn <caesar.chen@onebank.com.cn>
2016-08-16 15:42:49 +0800

    test 2

--- src/Base64.java
+++ src/Base64.java
@@ -92,7 +92,7 @@ public class Base64 {
     
     
     public static void main(String[] args) {
-		//test
+		//test 2
     	System.out.println(new String(Base64.decode("QUZCNTIwNTczNzQxMDIxQ0JFRTIyMkZC".toCharArray())));
     	
     	System.out.println(new String(Base64.encode("1".getBytes())));
